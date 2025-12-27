24048239b34122bd8836dc3afccdb90bc4fe4a76
bhashana99 <bhashanachamodya99@gmail.com>
2025-12-27 09:35:02 +0530

    initialize medicine inventory service

---
+++ back-end/medicine-inventory-service/src/main/java/com/findmymedi/medicine_inventory_service/MedicineInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.findmymedi.medicine_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ back-end/medicine-inventory-service/src/test/java/com/findmymedi/medicine_inventory_service/MedicineInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmymedi.medicine_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
