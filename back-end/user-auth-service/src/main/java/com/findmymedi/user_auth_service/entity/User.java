c35ece9608849ca33d20faf1d2d15a09feb31e35
bhashana99 <bhashanachamodya99@gmail.com>
2025-12-27 18:21:44 +0530

    add user entity to auth service

---
+++ back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/entity/User.java
@@ -0,0 +1,34 @@
+package com.findmymedi.user_auth_service.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+}
