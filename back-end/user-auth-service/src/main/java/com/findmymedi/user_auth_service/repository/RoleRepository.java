339357a6fe24436991e811b7b61492fa8f2a723c
bhashana99 <bhashanachamodya99@gmail.com>
2025-12-29 23:31:23 +0530

    change role name data type string to RoleType

--- back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/repository/RoleRepository.java
+++ back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/repository/RoleRepository.java
@@ -1,11 +1,12 @@
 package com.findmymedi.user_auth_service.repository;
 
 import com.findmymedi.user_auth_service.entity.Role;
+import com.findmymedi.user_auth_service.entity.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
-    Optional<Role> findByRoleName(String roleName);
+    Optional<Role> findByRoleName(RoleType roleName);
 
 }
