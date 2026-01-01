97b443a720d491d8f46ce61ec36960f259538e6b
bhashana99 <bhashanachamodya99@gmail.com>
2026-01-01 22:42:52 +0530

    allow csrf origin for actuator

--- back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/config/SecurityConfig.java
+++ back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/config/SecurityConfig.java
@@ -15,8 +15,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
