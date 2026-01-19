f125b3aa3f71b9084dd110a003635d9bddac0348
bhashana99 <bhashanachamodya99@gmail.com>
2026-01-19 23:26:59 +0530

    front url add to the env variable

--- back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/config/security/OAuth2LoginSuccessHandler.java
+++ back-end/user-auth-service/src/main/java/com/findmymedi/user_auth_service/config/security/OAuth2LoginSuccessHandler.java
@@ -1,11 +1,13 @@
 package com.findmymedi.user_auth_service.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    public OAuth2LoginSuccessHandler(){
-        setDefaultTargetUrl("http://localhost:4200/");
+
+    public OAuth2LoginSuccessHandler(@Value("${app.frontend.url}") String frontUrl){
+        setDefaultTargetUrl(frontUrl);
     }
 }
